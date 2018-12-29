734aaa04da8983cec5fc14dde532f89dba18cc75
BiggHeadd <153405814@qq.com>
2018-12-29 21:58:35 +0800

    finish xlsx

---
+++ chapter02/xlsx/read_xlsx.py
@@ -0,0 +1,22 @@
+#-*- coding: utf-8 -*-
+#Edited by bighead 18-12-29
+
+import xlrd
+
+file = "ch02-xlsxdata.xlsx"
+
+wb = xlrd.open_workbook(filename=file)
+
+ws = wb.sheet_by_name('Sheet1')
+
+dataset=[]
+
+for r in xrange(ws.nrows):
+    col=[]
+    for c in range(ws.ncols):
+            col.append(ws.cell(r,c).value)
+            dataset.append(col)
+
+print(dataset)
+#from pprint import pprint
+#pprint(dataset)
